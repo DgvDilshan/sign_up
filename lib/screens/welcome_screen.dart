d613f8fb66ecbf64c5e371ca52996043f81705bc
dgvdilshan@students.nsbm.ac.lk <vimukthidilshan23933@gmail.com>
2025-01-25 09:09:09 +0530

    eddit the welcome Screen my app

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+//import 'package:login_signup/screens/signin_screen.dart';
 import 'package:sign_up/screens/signin_screen.dart';
+//import 'package:login_signup/screens/signup_screen.dart';
+//import 'package:login_signup/theme/theme.dart';
+//import 'package:login_signup/widgets/custom_scaffold.dart';
+//import 'package:login_signup/widgets/welcome_button.dart';
 import 'package:sign_up/screens/signup_screen.dart';
 import 'package:sign_up/theme/theme.dart';
+import 'package:sign_up/widegets/custom_scaffold.dart';
 import 'package:sign_up/widegets/welcome_button.dart';
-import '../widegets/custom_scaffold.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  // Changed 'welcomeScreen' to 'WelcomeScreen'
   const WelcomeScreen({super.key});
 
   @override
@@ -15,52 +19,57 @@ class WelcomeScreen extends StatelessWidget {
       child: Column(
         children: [
           Flexible(
-            flex: 8,
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 0,
-                horizontal: 40.0,
-              ),
-              child: Center(
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: const TextSpan(children: [
-                    TextSpan(
-                      text: 'Welcome Back!\n',
-                      style: TextStyle(
-                        fontSize: 45.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    TextSpan(
-                      text:
-                          "\n Enter personal details to your employee account",
-                    ),
-                  ]),
+              flex: 8,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 0,
+                  horizontal: 40.0,
                 ),
-              ),
-            ),
-          ),
+                child: Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                            text: 'Welcome Back!\n',
+                            style: TextStyle(
+                              fontSize: 45.0,
+                              fontWeight: FontWeight.w600,
+                            )),
+                        TextSpan(
+                            text:
+                                '\nEnter personal details to your employee account',
+                            style: TextStyle(
+                              fontSize: 20,
+                              // height: 0,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              )),
           Flexible(
             flex: 1,
             child: Align(
               alignment: Alignment.bottomRight,
               child: Row(
                 children: [
-                  const Expanded(
-                      child: welcomeButton(
-                    buttonText: 'Sign In',
-                    onTap: SigninScreen(),
-                    color: Colors.transparent,
-                    textColor: Colors.white,
-                  )),
                   Expanded(
-                      child: welcomeButton(
-                    buttonText: 'Sign Up',
-                    onTap: const SignupScreen(),
-                    color: Colors.white,
-                    textColor: lightColorScheme.primary,
-                  )),
+                    child: WelcomeButton(
+                      buttonText: 'Sign in',
+                      onTap: SigninScreen(),
+                      color: Colors.transparent,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                  Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Sign up',
+                      onTap: const SignUpScreen(),
+                      color: Colors.white,
+                      textColor: lightColorScheme.primary,
+                    ),
+                  ),
                 ],
               ),
             ),
